2caef507a7b9beadad362e314e55e6d303c58afa
szarlih <39311561+szarlih@users.noreply.github.com>
2018-10-02 18:25:13 +0200

    basic files

---
+++ 2_interfejsy_kolekcje/Program.cs
@@ -0,0 +1,26 @@
+﻿/******************************************************************/
+/// <copyright file="Program.cs" company="Sharpro">
+/// Copyright (c) 2018 All Rights Reserved
+/// </copyright>
+/// <author>Jarosław Mielewski</author>
+/// <date>16:03</date>
+/******************************************************************/
+
+namespace _2_interfejsy_kolekcje
+{
+    using System;
+    using Extensions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySpecialCollection collection = new MySpecialCollection();
+            collection = new MySpecialCollection();
+            Console.WriteLine(collection);
+            Console.WriteLine(collection.LongerThanFiveElements());
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
